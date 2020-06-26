04b08c3c4d743d1f13b29ace9528cf2213aa30e2
GigabyteCrippler <jeffbonimississo@gmail.com>
2020-06-26 19:34:32 +0000

    api working

--- src/components/App.js
+++ src/components/App.js
@@ -18,76 +18,9 @@ class App extends React.Component {
   }
 
 
-    //weather
-    // get weather from api
-    // get 24 hour time
-  //   toggleHours() {
-  //     this.setState((prevState) => ({ TwentyFourHours: !prevState.TwentyFourHours }));
-  // }
-
-    // App.js
-    // write an switch statement , to change backgorund as time changes
-    // add toggle to switch between 24 hour and 12 hour
-
-
-    // ideas
-    // if greater than doesnt work use the 24 hour option
-    // can a switch statement be written for if its between this and this make it this
-    // a toggle button should be used to switch between 24 and 12
-    // how to keep it constantly refreshing (check clock app )
-    // make it refrsh ever second
-
-
-    // need to change multiple states simultaneously
-  //   startTime() {
-  //     this.timer = setInterval(() => {
-  //         this.setState(() => ({ Date:new Date().toDateString(),
-  //         Time :  new Date().toLocaleString()}));
-  //     }, 1000);
-  // }
-
-    // trial code
-    // if(time > 20 :00 pm ){
-    //   set the backgorund style to this night
-    // }
-    // else{
-    //   set the background style to day
-    // }
-    //
-    // timeToggleFunction(){
-    //   return this.state.toggle = false ? 12 hours display : 24 hour display
-    // }
-    //  if (togglebutton is clicked ){ then 24 hours } else {12 hours}
-    // look at how it is written in clock app
-
-
-    // css
-    // set css to push  all elements to the center
-    // the writing s should be in white bold
-    // time should be the largest
-    // location should be on top smaller writing next to the svg
-    // a diffrent classname for each element rendered
-    // try yo keep all styles in the index.css
-    // var d = new Date().gethours();
-    // // var n = d.getTime();
-    //  if(this.state.hours < 20 ) {
-    // day should be applied for the background
-    // // see how to apply styles inline
-    // // set the background image to this ;                 ;
-    //  } else {
-    // night should be applied for the background ;  ;
-    //  }
 
 
 
-  //   class Section extends Component {
-  //     render() {
-  //       return (
-  //         <section style={ sectionStyle }>
-  //         </section>
-  //     );
-  //   }
-  // }
 
    render(){
       return (
@@ -95,6 +28,7 @@ class App extends React.Component {
           <div className = 'center'>
             <p className = 'time'>{this.state.Time.substring(10,24)}</p>
             <p className = 'date'>{this.state.Date}</p>
+            <Weather/>
             <p>{this.state.Hours}</p>
           </div>
         </div>
@@ -103,3 +37,69 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+
+//weather
+// get weather from api
+// get 24 hour time
+//   toggleHours() {
+//     this.setState((prevState) => ({ TwentyFourHours: !prevState.TwentyFourHours }));
+// }
+
+// App.js
+// write an switch statement , to change backgorund as time changes
+// add toggle to switch between 24 hour and 12 hour
+
+
+// ideas
+// if greater than doesnt work use the 24 hour option
+// can a switch statement be written for if its between this and this make it this
+// a toggle button should be used to switch between 24 and 12
+// how to keep it constantly refreshing (check clock app )
+// make it refrsh ever second
+
+
+// need to change multiple states simultaneously
+
+
+// trial code
+// if(time > 20 :00 pm ){
+//   set the backgorund style to this night
+// }
+// else{
+//   set the background style to day
+// }
+//
+// timeToggleFunction(){
+//   return this.state.toggle = false ? 12 hours display : 24 hour display
+// }
+//  if (togglebutton is clicked ){ then 24 hours } else {12 hours}
+// look at how it is written in clock app
+
+
+// css
+
+// try yo keep all styles in the index.css
+// var d = new Date().gethours();
+// // var n = d.getTime();
+//  if(this.state.hours < 20 ) {
+// day should be applied for the background
+// // see how to apply styles inline
+// // set the background image to this ;                 ;
+//  } else {
+// night should be applied for the background ;  ;
+//  }
+
+
+
+
+
+
+// used to refresh time every second loo into other software
+//      startTime() {
+//        this.timer = setInterval(() => {
+//          this.setState(() => ({ Date:new Date().toDateString(),
+//          Time :  new Date().toLocaleString()}));
+//      }, 1000);
+//  }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,5 +1,5 @@
 import React , {Component} from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 
 class Weather extends React.Component {
   constructor(props) {
@@ -8,6 +8,7 @@ class Weather extends React.Component {
       latitude: null,
       longitude: null,
       data : null ,
+      temperature : null ,
       // locationTimezone : null ,
       // tempretureDegree : null ,
       // tempretureDescription : null ,
@@ -20,50 +21,28 @@ class Weather extends React.Component {
 
 
      componentDidMount(){
-        // this.apiRequest();
+        this.apiRequest();
         navigator.geolocation.getCurrentPosition(
-        position => this.setState({
+        position => console.log({
           latitude : position.coords.latitude ,
           longitude : position.coords.longitude,
+          position
         })
       )
     }
 
-   // apiRequest(){
-   //   fetch('https://api.darksky.net/forecast/8d2330fcf8903ed8a4fd1f337ec77849/5.662427699999999,-0.2137578')
-   //       .then(({ results }) => this.setState({ data: results }));
-   // }
-
-   async getDataAxios(){
-    const response =
-      await axios.get("https://api.darksky.net/forecast/8d2330fcf8903ed8a4fd1f337ec77849/5.662427699999999,-0.2137578")
-      .then(({ results }) => this.setState({ data: results }));
-  }
-
-
-//  console.log(response.data)
-   //'https://api.darksky.net/forecast/8d2330fcf8903ed8a4fd1f337ec77849/5.662427699999999,-0.2137578'
-   // add a setState and set the commented out variables
-   // use the latitude and longitude to get current country
-   // take it one by one
-   // revisit cors-anywhere
-   // go back to darkskys website
-   // see how the data is displayed ?
-   // skycons and darksky have diffrent ways of writing tempreture descriptions
-   // are the numbers inside for the long and latitude , should you switch them
-   // check how to console.log shows and see if the data youve fetched is working
-   //
+   apiRequest(){
+     const proxy = 'https://cors-anywhere.herokuapp.com/';
+     const api = `${proxy}https://api.darksky.net/forecast/8d2330fcf8903ed8a4fd1f337ec77849/${5.8142835999999996},${0.0746767}`;
+     fetch(api)
+     .then(response => response.json())
+     .then(data => console.log(data))
+     .then(result => this.setState({
+      // {this.state.temperature} : result.currently.temperature ,
+     }))
+   }
 
 
-
-   // getWeather() {
-   //     $.getJSON('https://api.darksky.net/forecast/8d2330fcf8903ed8a4fd1f337ec77849/${5.6131584000000005},${-0.212992}')
-   //       .then(({ results }) => console.log({ results }));
-   //   }
-
-   // apiFunction = () => {
-   //
-    // get `https://api.darksky.net/forecast/8d2330fcf8903ed8a4fd1f337ec77849/${5.662427699999999},${ -0.2137578}`
    //
    //   fetch(api)
    //       .then(response => {
@@ -80,6 +59,7 @@ class Weather extends React.Component {
 
 
    render(){
+     console.log('I was triggered during render')
       return (
         <div>
         <p>{this.state.latitude}</p>
